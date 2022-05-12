52aefa50681331b06115d5bfb1e942bc0aca4770
RichaPanjabi <richa.panjabi@weltec.ac.nz>
2022-05-13 10:11:11 +1200

    Update network.tf

--- network.tf
+++ network.tf
@@ -9,5 +9,5 @@ resource "azurerm_subnet" "subnet" {
   name                 = "book-subnet"
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = azurerm_resource_group.rg.name
-  address_prefix       = "10.0.10.0/24"
+  address_prefixes       = ["10.0.10.0/24"]
 }
