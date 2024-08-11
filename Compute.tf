a0d198e7de1e987bbee51fff3060e8aebdb30995
RichaPanjabi <richa.panjabi@weltec.ac.nz>
2024-08-11 19:01:49 +1200

    Update Compute.tf
    Changed the storage account name

--- Compute.tf
+++ Compute.tf
@@ -20,7 +20,7 @@ resource "azurerm_public_ip" "pip" {
 }
 
 resource "azurerm_storage_account" "stor" {
-  name                     = "bookstorpackt123"
+  name                     = "bookstorpacktrich"
   location                 = "australiaeast"
   resource_group_name      = azurerm_resource_group.rg.name
   account_tier             = "Standard"
